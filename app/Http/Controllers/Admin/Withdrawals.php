f837673936b8b675de368e29998992a96c1ab042
Anon Dev <michaelerastus9@gmail.com>
2025-06-19 14:35:36 +0100

    Updated

--- app/Http/Controllers/Admin/Withdrawals.php
+++ app/Http/Controllers/Admin/Withdrawals.php
@@ -87,9 +87,7 @@ class Withdrawals extends Controller
             User::where('id',$investor->id)->update($dataUser);
 
             $userMessage = "
-                $<b>".$withdrawal->amount." </b> has been successfully sent to your ".$withdrawal->asset."  -
-                ".$withdrawal->details.".
-                Transaction Batch is ".sha1(rand(100000000,900000000))."
+              Your Withdrawal of $<b>".$withdrawal->amount." </b> has been successfully sent to your wallet.
             ";
             $investor->notify(new DepositMail($investor,$userMessage,'Withdrawal Processed'));
             //send mail to user
